35980f1a53048f5b48f8a481c3c684a65cba1005
François Roudier <bigfanch@free.fr>
2024-07-25 11:42:53 +0200

    Pen and Fill proprerties functional

--- PainterApplication/mainwindow.cpp
+++ PainterApplication/mainwindow.cpp
@@ -4,17 +4,73 @@
 #include <QGraphicsPixmapItem>
 #include <QPixmap>
 #include <QGraphicsScene>
+#include <QColorDialog>
 //#include <QMimeData>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow),
-    draggedShape(nullptr){ // à deplacer sur class board
+    draggedShape(nullptr)
+{ // à deplacer sur class board
     ui->setupUi(this);
     setAcceptDrops(true);
     setupShapes();
     QGraphicsScene * scene(new QGraphicsScene(this));// use smart pointer
     ui->libraryView->setScene(scene);
+
+    _currentPen.setColor(Qt::black);
+    _currentPen.setWidth(2);
+    _currentPen.setStyle(Qt::SolidLine);
+    ui->penColorPB->setStyleSheet(QString("background-color : %1").arg(_currentPen.color().name()));
+
+    _currentBrush.setColor(Qt::black);
+    _currentBrush.setStyle(Qt::SolidPattern);
+    ui->fillColorPB->setStyleSheet(QString("background-color : %1").arg(_currentBrush.color().name()));
+
+    ui->penDirectColor1->setFixedSize(QSize(24, 24));
+    ui->penDirectColor2->setFixedSize(QSize(24, 24));
+    ui->penDirectColor3->setFixedSize(QSize(24, 24));
+    ui->penDirectColor4->setFixedSize(QSize(24, 24));
+    ui->penDirectColor5->setFixedSize(QSize(24, 24));
+    ui->penDirectColor6->setFixedSize(QSize(24, 24));
+    ui->penDirectColor7->setFixedSize(QSize(24, 24));
+    ui->penDirectColor8->setFixedSize(QSize(24, 24));
+    ui->penDirectColor9->setFixedSize(QSize(24, 24));
+    ui->penDirectColor10->setFixedSize(QSize(24, 24));
+    ui->penDirectColor11->setFixedSize(QSize(24, 24));
+    ui->penDirectColor12->setFixedSize(QSize(24, 24));
+    ui->penDirectColor13->setFixedSize(QSize(24, 24));
+    ui->penDirectColor14->setFixedSize(QSize(24, 24));
+    /* First row */
+    ui->penDirectColor1->setStyleSheet(QString("background-color : %1").arg("#ffffff"));
+    connect(ui->penDirectColor1, &QPushButton::clicked, this, [=](){ this->on_directColor_clicked(QColor("#ffffff"));});
+    ui->penDirectColor2->setStyleSheet(QString("background-color : %1").arg("#ffff00"));
+    connect(ui->penDirectColor2, &QPushButton::clicked, this, [=](){ this->on_directColor_clicked(QColor("#ffff00"));});
+    ui->penDirectColor3->setStyleSheet(QString("background-color : %1").arg("#00ff00"));
+    connect(ui->penDirectColor3, &QPushButton::clicked, this, [=](){ this->on_directColor_clicked(QColor("#00ff00"));});
+    ui->penDirectColor4->setStyleSheet(QString("background-color : %1").arg("#00ffff"));
+    connect(ui->penDirectColor4, &QPushButton::clicked, this, [=](){ this->on_directColor_clicked(QColor("#00ffff"));});
+    ui->penDirectColor5->setStyleSheet(QString("background-color : %1").arg("#0000ff"));
+    connect(ui->penDirectColor5, &QPushButton::clicked, this, [=](){ this->on_directColor_clicked(QColor("#0000ff"));});
+    ui->penDirectColor6->setStyleSheet(QString("background-color : %1").arg("#ff00ff"));
+    connect(ui->penDirectColor6, &QPushButton::clicked, this, [=](){ this->on_directColor_clicked(QColor("#ff00ff"));});
+    ui->penDirectColor7->setStyleSheet(QString("background-color : %1").arg("#ff0000"));
+    connect(ui->penDirectColor7, &QPushButton::clicked, this, [=](){ this->on_directColor_clicked(QColor("#ff0000"));});
+    /* Secoond row */
+    ui->penDirectColor8->setStyleSheet(QString("background-color : %1").arg("#000000"));
+    connect(ui->penDirectColor8, &QPushButton::clicked, this, [=](){ this->on_directColor_clicked(QColor("#000000"));});
+    ui->penDirectColor9->setStyleSheet(QString("background-color : %1").arg("#c0c000"));
+    connect(ui->penDirectColor9, &QPushButton::clicked, this, [=](){ this->on_directColor_clicked(QColor("#c0c000"));});
+    ui->penDirectColor10->setStyleSheet(QString("background-color : %1").arg("#00c000"));
+    connect(ui->penDirectColor10, &QPushButton::clicked, this, [=](){ this->on_directColor_clicked(QColor("#00c000"));});
+    ui->penDirectColor11->setStyleSheet(QString("background-color : %1").arg("#00c0c0"));
+    connect(ui->penDirectColor11, &QPushButton::clicked, this, [=](){ this->on_directColor_clicked(QColor("#00c0c0"));});
+    ui->penDirectColor12->setStyleSheet(QString("background-color : %1").arg("#0000c0"));
+    connect(ui->penDirectColor12, &QPushButton::clicked, this, [=](){ this->on_directColor_clicked(QColor("#0000c0"));});
+    ui->penDirectColor13->setStyleSheet(QString("background-color : %1").arg("#c000c0"));
+    connect(ui->penDirectColor13, &QPushButton::clicked, this, [=](){ this->on_directColor_clicked(QColor("#c000c0"));});
+    ui->penDirectColor14->setStyleSheet(QString("background-color : %1").arg("#c00000"));
+    connect(ui->penDirectColor14, &QPushButton::clicked, this, [=](){ this->on_directColor_clicked(QColor("#c00000"));});
 
     // Load icons from resources and create QGraphicsPixmapItem for each icon
     QPixmap pixmap1(":/images/ellipse.png");
@@ -129,5 +185,82 @@ void MainWindow::mouseReleaseEvent(QMouseEvent *event) {
 void MainWindow::on_actionQuit_triggered()
 {
     QApplication::quit();
+}
+
+void MainWindow::on_penColorPB_clicked()
+{
+    QColor color = QColorDialog::getColor(_currentPen.color(), this);
+
+    if ( color.isValid() ) {
+        _currentPen.setColor(color);
+        ui->penColorPB->setStyleSheet(QString("background-color : %1").arg(color.name()));
+    }
+}
+
+void MainWindow::on_penWidthSB_valueChanged(int value)
+{
+    _currentPen.setWidth(value);
+}
+
+void MainWindow::on_penStyleCB_currentIndexChanged(int index)
+{
+    switch(index) {
+    case 0:
+        _currentPen.setStyle(Qt::SolidLine);
+        break;
+    case 1:
+        _currentPen.setStyle(Qt::DashLine);
+        break;
+    case 2:
+        _currentPen.setStyle(Qt::DotLine);
+        break;
+    case 3:
+        _currentPen.setStyle(Qt::DashDotLine);
+        break;
+    default:
+        break;
+    }
+}
+
+void MainWindow::on_directColor_clicked(const QColor& color)
+{
+    _currentPen.setColor(color);
+    ui->penColorPB->setStyleSheet(QString("background-color : %1").arg(color.name()));
+}
+
+void MainWindow::on_fillColorPB_clicked()
+{
+    QColor color = QColorDialog::getColor(_currentBrush.color(), this);
+
+    if ( color.isValid() ) {
+        _currentBrush.setColor(color);
+        ui->fillColorPB->setStyleSheet(QString("background-color : %1").arg(color.name()));
+    }
+}
+
+void MainWindow::on_fillStyleCB_currentIndexChanged(int index)
+{
+    switch(index) {
+    case 0: // Solid
+        _currentBrush.setStyle(Qt::SolidPattern);
+        break;
+    case 1: // Dense
+        _currentBrush.setStyle(Qt::Dense4Pattern);
+        break;
+    case 2:
+        _currentBrush.setStyle(Qt::HorPattern);
+        break;
+    case 3:
+        _currentBrush.setStyle(Qt::VerPattern);
+        break;
+    case 4:
+        _currentBrush.setStyle(Qt::CrossPattern);
+        break;
+    case 5:
+        _currentBrush.setStyle(Qt::NoBrush);
+        break;
+    default:
+        break;
+    }
 }
 
--- PainterApplication/mainwindow.h
+++ PainterApplication/mainwindow.h
@@ -21,6 +21,28 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+    /**
+     * @brief getcurrentPen()
+     *     An accessor to the current QPen property
+     *     (The outline for the new Shapes created)
+     * @return the current QPen
+     */
+    const QPen& getcurrentPen() const
+    {
+        return _currentPen;
+    }
+
+    /**
+     * @brief getcurrentBrush()
+     *     An accessor to the current QBrush property
+     *     (The filling for the new Shapes created)
+     * @return
+     */
+    const QBrush& getcurrentBrush() const
+    {
+        return _currentBrush;
+    }
+
 protected:
 
     void paintEvent(QPaintEvent *event) override;
@@ -31,7 +53,42 @@ protected:
     // void dropEvent(QDropEvent *event) override;
 
 private slots:
+
+    /**
+     * @brief on_actionQuit_triggered
+     * slot called when action "Quit" is triggered
+     */
     void on_actionQuit_triggered();
+
+    /**
+     * @brief on_penColorPB_clicked
+     * slot triggered when pressing on the pen color button
+     */
+    void on_penColorPB_clicked();
+
+    /**
+     * @brief on_penWidthSB_valueChanged
+     * slot triggered when using the pen width spinbox
+     * @param width
+     * the width value selected
+     */
+    void on_penWidthSB_valueChanged(int width);
+
+    /**
+     * @brief on_penStyleCB_currentIndexChanged
+     * slot triggered when changing the pen style combobox
+     * @param index
+     * index of the style selected (0 to 3)
+     */
+    void on_penStyleCB_currentIndexChanged(int index);
+
+private:
+    void on_directColor_clicked(const QColor&);
+
+private slots:
+    void on_fillColorPB_clicked();
+
+    void on_fillStyleCB_currentIndexChanged(int index);
 
 private:
     Ui::MainWindow *ui;
@@ -40,6 +97,8 @@ private:
     QPoint lastMousePosition;
     //bool drawing;
 
+    QPen _currentPen;
+    QBrush _currentBrush;
 
     void setupShapes();
 };
