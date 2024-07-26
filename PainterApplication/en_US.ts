e3f7ca828383565d2b8041ef9c23e849ee6bd212
François Roudier <bigfanch@free.fr>
2024-07-26 13:37:35 +0200

    Added translations

---
+++ PainterApplication/en_US.ts
@@ -0,0 +1,336 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US" sourcelanguage="en_US">
+<context>
+    <name>Board</name>
+    <message>
+        <location filename="board.ui" line="14"/>
+        <source>board</source>
+        <translation>board</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation>Painter Application</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="26"/>
+        <source>Pen Properties</source>
+        <translation>Pen Properties</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="34"/>
+        <source>Pen Color :</source>
+        <translation>Pen Color:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="169"/>
+        <source>Pen Width :</source>
+        <translation>Pen Width:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="190"/>
+        <source>Pen Style :</source>
+        <translation>Pen Style:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="198"/>
+        <location filename="mainwindow.ui" line="280"/>
+        <source>Solid</source>
+        <translation>Solid</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="208"/>
+        <source>Dashed</source>
+        <translation>Dashed</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="218"/>
+        <source>Dotted</source>
+        <translation>Dotted</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="228"/>
+        <source>Dot Dashed</source>
+        <translation>Dot Dashed</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="246"/>
+        <source>Fill Properties</source>
+        <translation>Fill Properties</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="254"/>
+        <source>Fill Color :</source>
+        <translation>Fill Color:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="272"/>
+        <source>Fill Style :</source>
+        <translation>Fill Style:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="285"/>
+        <source>Dense</source>
+        <translation>Dense</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="290"/>
+        <source>Horizontal Lines</source>
+        <translation>Horizontal Lines</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="295"/>
+        <source>Vertical Lines</source>
+        <translation>Vertical Lines</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="300"/>
+        <source>Cross Pattern</source>
+        <translation>Cross Pattern</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="305"/>
+        <source>&lt;none&gt;</source>
+        <translation>&lt;none&gt;</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="318"/>
+        <source>Canvas Properties</source>
+        <translation>Canvas Properties</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="324"/>
+        <source>Show Grid</source>
+        <translation>Show Grid</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="334"/>
+        <source>Center Scene</source>
+        <translation>Center Scene</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="343"/>
+        <source>Scene Background :</source>
+        <translation>Scene Background:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="362"/>
+        <source>Library</source>
+        <translation>Library</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="430"/>
+        <source>File</source>
+        <translation>File</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="439"/>
+        <source>Edit</source>
+        <translation>Edit</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="450"/>
+        <source>Tools</source>
+        <translation>Tools</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="461"/>
+        <source>Help</source>
+        <translation>Help</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="475"/>
+        <source>toolBar</source>
+        <translation>toolBar</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="506"/>
+        <source>Add Image</source>
+        <translation>Add Image</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="509"/>
+        <source>Add an image to the drawing</source>
+        <translation>Add an Image to the Drawing</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="521"/>
+        <source>Save</source>
+        <translation>Save</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="524"/>
+        <source>Save the document</source>
+        <translation>Save the Document</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="536"/>
+        <source>Load</source>
+        <translation>Load</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="539"/>
+        <source>Load a document</source>
+        <translation>Load A Document</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="551"/>
+        <source>Quit</source>
+        <translation>Quit</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="554"/>
+        <source>Quit application</source>
+        <translation>Quit Application</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="566"/>
+        <source>Copy</source>
+        <translation>Copy</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="569"/>
+        <source>Copy an object into clipboard</source>
+        <translation>Copy an Object into Clipboard</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="581"/>
+        <source>Cut</source>
+        <translation>Cut</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="584"/>
+        <source>Cut an object into clipboard</source>
+        <translation>Cut an Object into Clipboard</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="596"/>
+        <source>Paste</source>
+        <translation>Paste</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="599"/>
+        <source>Paste object in clipboard to drawing area</source>
+        <translation>Paste Object in Clipboard to Drawing Area</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="611"/>
+        <source>Undo</source>
+        <translation>Undo</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="614"/>
+        <source>Undo the last action</source>
+        <translation>Undo the last action</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="626"/>
+        <source>Redo</source>
+        <translation>Redo</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="629"/>
+        <source>Redo the last undone action</source>
+        <translation>Redo the last undone action</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="644"/>
+        <source>Select</source>
+        <translation>Select</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="647"/>
+        <source>Select, move and resize an object</source>
+        <translation>Select, move and resize an Object</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="662"/>
+        <source>Rectangle</source>
+        <translation>Rectangle</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="665"/>
+        <source>Draw a Rectangle</source>
+        <translation>Draw a Rectangle</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="680"/>
+        <source>Pen</source>
+        <translation>Pen</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="683"/>
+        <source>Free-hand Pen drawing</source>
+        <translation>Free-hand Pen Drawing</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="698"/>
+        <source>Eraser</source>
+        <translation>Eraser</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="701"/>
+        <source>Use eraser on Free-hand drawings</source>
+        <translation>Use Eraser on Free-hand Drawing</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="716"/>
+        <source>Ellipse</source>
+        <translation>Ellipse</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="719"/>
+        <source>Draw an Ellipse</source>
+        <translation>Draw an Ellipse</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="734"/>
+        <source>Star</source>
+        <translation>Star</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="737"/>
+        <source>Draw an Five-points Star</source>
+        <translation>Draw a Five points Star</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="749"/>
+        <source>About</source>
+        <translation>About</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="757"/>
+        <source>User Manual</source>
+        <translation>User Manual</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="760"/>
+        <source>Open the User Manual</source>
+        <translation>Open The User Manual</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="768"/>
+        <source>Language Settings</source>
+        <translation>Language Settings</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="771"/>
+        <source>Change Language of the Application</source>
+        <translation>Change the Language of the Application</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="333"/>
+        <source>Save document</source>
+        <translation>Save Document</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="365"/>
+        <source>Open document</source>
+        <translation>Open Document</translation>
+    </message>
+</context>
+</TS>
