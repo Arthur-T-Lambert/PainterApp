38795c69cc2dd17bcacc088ab25d8c0cc55c228f
Arthur Lambert <arthurlambert95@gmail.com>
2024-07-26 10:25:31 +0200

    Modify Dessin virtual class to add the draw method

--- PainterApplication/dessin.h
+++ PainterApplication/dessin.h
@@ -2,6 +2,7 @@
 #define DESSIN_H
 
 #include <QPoint>
+#include <QPainter>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -22,21 +23,27 @@ public:
     Dessin();
 
     /**
-     * \brief Dessin.
+     * \brief Move method.
      * \param delta Move vector from the old position to the new position.
      */
     virtual void move(const QPoint &delta) = 0;
 
     /**
+     * \brief Draw method, draw the dessin to a QPainter
+     * \param painter The QPainter to draw the dessin.
+     */
+    virtual void draw(QPainter *painter) = 0;
+
+    /**
      * \brief Getter fot the position of a Dessin.
      * \return The position of the Dessin.
      */
-    virtual QPoint getPosition() const { return QPoint(); }
+    virtual QPoint getPosition() const = 0;
     /**
      * \brief Setter fot the position of a Dessin.
      * \param The new position of the Dessin.
      */
-    virtual void SetPosition(const QPoint& point) { Q_UNUSED(point);}
+    virtual void SetPosition(const QPoint& point) = 0;
 };
 
 #endif // DESSIN_H
